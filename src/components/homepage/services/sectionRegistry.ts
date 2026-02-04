da09780d6513dfd65eb1fc65014dcafb5d643e27
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2026-02-04 17:01:02 -0600

    Fix import

--- src/components/homepage/services/sectionRegistry.ts
+++ src/components/homepage/services/sectionRegistry.ts
@@ -2,11 +2,11 @@ import React from 'react';
 
 import HeroSection from '@/src/components/homepage/HeroSection';
 import ServiceCardsSection from '@/src/components/homepage/services/ServiceCardsSection';
-import PrimaryCTASection from '@src/components/homepage/PrimaryCTASection';
+import PrimaryCTASection from '@/src/components/homepage/PrimaryCTASection';
 import ValuePropsSection from '@/src/components/homepage/ValuePropsSection';
 import ReviewsSection from '@/src/components/homepage/ReviewsSection';
 import GallerySection from '@/src/components/homepage/GallerySection';
-import FaqSection from '@src/components/homepage/Faq/FaqSection';
+import FaqSection from '@/src/components/homepage/Faq/FaqSection';
 
 import { HomepageSectionType } from '@src/config/tiers';
 export type HomepageSectionComponent = React.ComponentType<{}>;
@@ -19,7 +19,7 @@ export const homepageSectionRegistry: Record<
   hero: HeroSection,
   'service-cards': ServiceCardsSection,
   cta: PrimaryCTASection,
-  'value-props': ValuePropsSection,
+  'value-props': ValuePropsSection,g
   reviews: ReviewsSection,
   gallery: GallerySection,
   faq: FaqSection
