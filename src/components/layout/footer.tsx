fedf25bfc61ab27d682a829d7552139b06865295
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2026-02-10 14:08:32 -0600

    removed comment

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,8 +3,6 @@ import FooterContactBar from '@/src/config/footer/footerContactBar';
 export default function Footer() {
   return (
     <footer>
-      {/* your existing footer markup here */}
-
       <FooterContactBar />
     </footer>
   );
